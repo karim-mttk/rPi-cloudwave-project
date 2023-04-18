4aec219e80bbb1abc7efe94d00293ce839ae2309
AntonHerRing <anton.herdin@gmail.com>
2023-04-18 14:19:34 +0200

    added 2. coding

--- Buttons.py
+++ Buttons.py
@@ -5,18 +5,18 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.IN)  # C
-GPIO.setup(4, GPIO.IN)  # D
-GPIO.setup(4, GPIO.IN)  # E
-GPIO.setup(4, GPIO.IN)  # F
-GPIO.setup(4, GPIO.IN)  # G
-GPIO.setup(4, GPIO.IN)  # A
-GPIO.setup(4, GPIO.IN)  # B
-GPIO.setup(4, GPIO.IN)  # C#
-GPIO.setup(4, GPIO.IN)  # D#
-GPIO.setup(4, GPIO.IN)  # F#
-GPIO.setup(4, GPIO.IN)  # G#
-GPIO.setup(4, GPIO.IN)  # A#
+GPIO.setup(7, GPIO.IN)  # C GPIO 4
+GPIO.setup(13, GPIO.IN)  # D GPIO 27
+GPIO.setup(15, GPIO.IN)  # E GPIO 22
+GPIO.setup(29, GPIO.IN)  # F GPIO 5
+GPIO.setup(31, GPIO.IN)  # G GPIO 6
+GPIO.setup(37, GPIO.IN)  # A GPIO 26
+GPIO.setup(16, GPIO.IN)  # B GPIO 23
+GPIO.setup(22, GPIO.IN)  # C# GPIO 25
+GPIO.setup(3, GPIO.IN)  # D# GPIO 2
+GPIO.setup(5, GPIO.IN)  # F# GPIO 3
+GPIO.setup(26, GPIO.IN)  # G# GPIO 7
+GPIO.setup(24, GPIO.IN)  # A# GPIO 8
 
 while True:
     if GPIO.input(2) == 0:
