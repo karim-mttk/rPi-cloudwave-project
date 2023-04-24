1e7ca80a78d469f82b12804611dc85c344b09e26
AntonHerRing <anton.herdin@gmail.com>
2023-04-24 16:20:50 +0200

    can play files in order from firebase

--- Firebase.py
+++ Firebase.py
@@ -3,6 +3,9 @@ from firebase_admin import credentials, storage
 
 import pygame
 
+# initialize Pygame mixer for playing sound files
+pygame.mixer.init()
+
 cred = credentials.Certificate(r"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\firebase key\cloudwave-test-firebase-adminsdk-ejn2w-6a4e295421.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'cloudwave-test.appspot.com'
@@ -13,7 +16,7 @@ bucket = storage.bucket()
 
 # specify the path to the audio file on your local machine
 # change to RPi storage path later
-file_path = r'C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\Sound test/C_Major_Chord.wav'
+# file_path = r'C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\Sound test/C_Major_Chord.wav'
 
 # download index number
 index_path = 'index.txt'
@@ -38,33 +41,33 @@ Chords = [{"note": "C"},
           {"note": "G#"},
           {"note": "A#"}]
 
-
 # download all chords and put in local storage
+all_chords = {}
+IndexPath = 0
 for note in Chords:
     # specify the path to the audio file in Firebase Storage
-    storage_path = rf"sounds{index}/folder/{note['note']}.wav"
+    storage_path = rf"sounds{index}/folder/{note['note']}.mp3"
 
-    temp_file = rf"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp/{note['note']}.wav"
+    # download the sound file to a temporary file
+    # change to RPi storage path later
+    temp_file = rf"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp/{note['note']}.mp3"
     blob = bucket.blob(storage_path)
     blob.download_to_filename(temp_file)
 
+    # load the sound file into Pygame mixer and save it
+    all_chords[IndexPath] = pygame.mixer.Sound(temp_file)
+    IndexPath += 1
+# print(all_chords)
 # upload the audio file to Firebase Storage
 # blob = bucket.blob(storage_path)
 # blob.upload_from_filename(file_path)
 
-# download the sound file to a temporary file
-# change to RPi storage path later
-temp_file = r'C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp/sound-file.mp3'
-blob = bucket.blob(storage_path)
-blob.download_to_filename(temp_file)
+# play all sounds, wait for each sound to finish playing
+i = 0
+for note in Chords:
+    print(note['note'])
+    all_chords[i].play()
+    i += 1
+    while pygame.mixer.get_busy():
+        pass
 
-# initialize Pygame mixer for playing sound files
-pygame.mixer.init()
-
-# load the sound file into Pygame mixer and play it
-sound = pygame.mixer.Sound(temp_file)
-sound.play()
-
-# wait for the sound to finish playing
-while pygame.mixer.get_busy():
-    pass
