ba38e3e6eae8ba9767204edab8fc1b967535039e
AntonHerRing <anton.herdin@gmail.com>
2023-04-25 15:43:02 +0200

    made it more Rpi compatible

--- Firebase.py
+++ Firebase.py
@@ -1,61 +1,45 @@
 import firebase_admin
 from firebase_admin import credentials, storage
-
 import pygame
 
 # initialize Pygame mixer for playing sound files
 pygame.mixer.init()
 
-cred = credentials.Certificate(r"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\firebase key\cloudwave-test-firebase-adminsdk-ejn2w-6a4e295421.json")
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'cloudwave-test.appspot.com'
-})
+cred = credentials.Certificate(r"firebasekey.json")
+firebase_admin.initialize_app(cred, {'storageBucket': 'cloudwave-test.appspot.com'})
 
 # create a Firebase Storage client
 bucket = storage.bucket()
 
-# specify the path to the audio file on your local machine
-# change to RPi storage path later
-# file_path = r'C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\Sound test/C_Major_Chord.wav'
-
 # download index number
 index_path = 'index.txt'
 # change to RPi storage path later
-index_temp_file = r'C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp/index.txt'
+index_temp_file = r'index.txt'
 blob = bucket.blob(index_path)
 blob.download_to_filename(index_temp_file)
 with open(index_temp_file) as file:
     index = file.read()
 
 # alternative method for selecting chords
-Chords = [{"note": "C"},
-          {"note": "D"},
-          {"note": "E"},
-          {"note": "F"},
-          {"note": "G"},
-          {"note": "A"},
-          {"note": "B"},
-          {"note": "C#"},
-          {"note": "D#"},
-          {"note": "F#"},
-          {"note": "G#"},
-          {"note": "A#"}]
+Chords = [{"note": "C"}, {"note": "D"}, {"note": "E"}, {"note": "F"},
+          {"note": "G"}, {"note": "A"}, {"note": "B"}, {"note": "C#"},
+          {"note": "D#"}, {"note": "F#"}, {"note": "G#"}, {"note": "A#"}]
 
 # download all chords and put in local storage
-all_chords = {}
+all_chords = []
 IndexPath = 0
 for note in Chords:
     # specify the path to the audio file in Firebase Storage
-    storage_path = rf"sounds{index}/folder/{note['note']}.mp3"
+    storage_path = rf"sounds{index}/folder/{note['note']}.wav"
 
     # download the sound file to a temporary file
     # change to RPi storage path later
-    temp_file = rf"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp/{note['note']}.mp3"
+    temp_file = rf"/home/pi/Desktop/programming/cloudwave/sound/{note['note']}.wav"
     blob = bucket.blob(storage_path)
     blob.download_to_filename(temp_file)
 
     # load the sound file into Pygame mixer and save it
-    all_chords[IndexPath] = pygame.mixer.Sound(temp_file)
+    all_chords.append(pygame.mixer.Sound(temp_file))
     IndexPath += 1
 
 # upload the audio file to Firebase Storage
