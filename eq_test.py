895c80137c2f1ca427144a48d5599113ef4177c6
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-05-03 11:25:01 +0200

    Add files via upload

---
+++ eq_test.py
@@ -0,0 +1,24 @@
+from pydub import AudioSegment
+
+Chords = [{"note": "C"}, {"note": "D"}, {"note": "E"}, {"note": "F"},
+          {"note": "G"}, {"note": "A"}, {"note": "B"}, {"note": "C#"},
+          {"note": "D#"}, {"note": "F#"}, {"note": "G#"}, {"note": "A#"}]
+
+# Define the gain adjustments in decibels
+
+def equalizerSet():
+    bass = 20
+    mid = 1
+    treble = 1
+    for note in Chords:
+        # Load the audio file
+        sound = AudioSegment.from_file(rf"/home/pi/Desktop/programming/cloudwave/sound/{note['note']}.wav", format="wav")
+    
+        # Apply the gain adjustments to the audio
+        adjusted_sound = sound \
+            .low_pass_filter(bass) \
+            .high_pass_filter(treble) \
+            .apply_gain(mid)
+            
+        # Export the adjusted sound
+        adjusted_sound.export(rf"/home/pi/Desktop/programming/cloudwave/sound/{note['note']}.wav", format="wav")
