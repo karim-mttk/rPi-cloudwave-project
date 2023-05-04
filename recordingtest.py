be9bb1ea9e37db5ace9be6ae598c914136e79ae9
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-05-04 09:33:48 +0200

    Update recordingtest.py

--- recordingtest.py
+++ recordingtest.py
@@ -9,39 +9,40 @@ GPIO.setup(17, GPIO.IN)
 # Set up ALSA mixer
 mixer = alsaaudio.Mixer(control='Headphone', cardindex=2)
 
-# Set up audio parameters
-FORMAT = alsaaudio.PCM_FORMAT_S16_LE
-CHANNELS = 2
-RATE = 44100
-CHUNK_SIZE = 1024
-
-# Define filename and create wave file
-filename = 'recording.wav'
-wave_file = wave.open(filename, 'wb')
-wave_file.setnchannels(CHANNELS)
-wave_file.setsampwidth(2)
-wave_file.setframerate(RATE)
-
-# Define callback function to start/stop recording
-def start_stop_recording(channel):
-    if GPIO.input(channel):
-        print('Recording started')
-        with alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, cardindex=2) as capture:
-            capture.setchannels(CHANNELS)
-            capture.setrate(RATE)
-            capture.setformat(FORMAT)
-            capture.setperiodsize(CHUNK_SIZE)
-            while GPIO.input(channel):
-                l, data = capture.read()
-                if l:
-                    wave_file.writeframes(data)
-    else:
-        print('Recording stopped')
-        wave_file.close()
+# Define callback function to adjust volume
+def change_volume(channel):
+    volume = alsaaudio.Mixer(control='Headphone', cardindex=2).getvolume()[0]
+    if channel == 27 and volume < 100:
+        mixer.setvolume(volume+1)
 
 # Add event detection to GPIO
-GPIO.add_event_detect(17, GPIO.BOTH, callback=start_stop_recording)
+GPIO.add_event_detect(27, GPIO.RISING, callback=change_volume)
 
-# Keep script running
+# Set up audio recording
+card = 'bcm2835 Headphones'
+device = 'default'
+wav_output = wave.open('recording.wav', 'wb')
+wav_output.setnchannels(2)
+wav_output.setsampwidth(2)
+wav_output.setframerate(44100)
+
+# Record audio when GPIO17 is high
 while True:
-    pass
+    if GPIO.input(17) == GPIO.HIGH:
+        # Start recording
+        print("Recording started")
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, card=card, device=device)
+        inp.setchannels(2)
+        inp.setrate(44100)
+        inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp.setperiodsize(1024)
+        frames = []
+        while GPIO.input(17) == GPIO.HIGH:
+            l, data = inp.read()
+            if l:
+                frames.append(data)
+                wav_output.writeframes(data)
+        # Stop recording
+        print("Recording stopped")
+        inp.close()
+        wav_output.close()
