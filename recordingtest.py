6e0871b3764097ad4e2547431ceafee99a965cd8
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-05-04 11:20:58 +0200

    Update recordingtest.py

--- recordingtest.py
+++ recordingtest.py
@@ -1,10 +1,6 @@
-import RPi.GPIO as GPIO
 import alsaaudio
 import wave
-
-# Set up GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
+import time
 
 # Set up ALSA mixer
 mixer = alsaaudio.Mixer(control='Headphone', cardindex=2)
@@ -22,28 +18,17 @@ wave_file.setnchannels(CHANNELS)
 wave_file.setsampwidth(2)
 wave_file.setframerate(RATE)
 
-# Define function to start recording
-def start_recording(channel):
-    print('Recording started')
-    with alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, cardindex=2) as capture:
-        capture.setchannels(CHANNELS)
-        capture.setrate(RATE)
-        capture.setformat(FORMAT)
-        capture.setperiodsize(CHUNK_SIZE)
-        while GPIO.input(channel):
-            l, data = capture.read()
-            if l:
-                wave_file.writeframes(data)
+# Start recording
+with alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, cardindex=2) as capture:
+    capture.setchannels(CHANNELS)
+    capture.setrate(RATE)
+    capture.setformat(FORMAT)
+    capture.setperiodsize(CHUNK_SIZE)
+    start_time = time.time()
+    while time.time() - start_time < 10:
+        l, data = capture.read()
+        if l:
+            wave_file.writeframes(data)
 
-# Define function to stop recording
-def stop_recording(channel):
-    print('Recording stopped')
-    wave_file.close()
-
-# Add event detection to GPIO
-GPIO.add_event_detect(17, GPIO.RISING, callback=start_recording)
-GPIO.add_event_detect(17, GPIO.FALLING, callback=stop_recording)
-
-# Keep script running
-while True:
-    pass
+# Stop recording and close wave file
+wave_file.close()
