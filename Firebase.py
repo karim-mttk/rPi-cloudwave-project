6089d4fcdfb66339d0fd23af9d92c4066915d8fc
AntonHerRing <anton.herdin@gmail.com>
2023-05-04 13:14:59 +0200

    updated for testing

--- Firebase.py
+++ Firebase.py
@@ -2,12 +2,10 @@ import firebase_admin
 from firebase_admin import credentials, storage, db, auth
 
 import pygame
-import numpy as np
-# import soundcard as sc
 import pyttsx3
-from scipy.io.wavfile import write
-# from eq_test import equalizerSet
-# import alsaaudio
+from eq_test import equalizerSet
+from python_Scr_C_record import record
+from raw-to-wav-converter import converter
 
 import pyaudio
 import wave
@@ -18,19 +16,13 @@ CHANNELS = 2
 RATE = 44100
 CHUNK = 1024
 
-# Open the soundcard device for recording
-# input_device = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
-
-# Print the user's email address
-# print(user.email)
-
 # initialize Pygame mixer for playing sound files
 pygame.init()
 pygame.mixer.init(frequency=44100, size=16, channels=2, buffer=4096)
 
 
-cred = credentials.Certificate(r"firebasekey.json")
-# cred = credentials.Certificate(r"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\firebase key\cloudwave-test-firebase-adminsdk-ejn2w-6a4e295421.json")
+#cred = credentials.Certificate(r"firebasekey.json")
+cred = credentials.Certificate(r"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\firebase key\cloudwave-test-firebase-adminsdk-ejn2w-6a4e295421.json")
 firebase_admin.initialize_app(cred, {'storageBucket': 'cloudwave-test.appspot.com', 'databaseURL': 'https://cloudwave-test-default-rtdb.europe-west1.firebasedatabase.app'})
 
 # create a Firebase Storage client
@@ -66,7 +58,7 @@ def validate_synth_password():
                 break
             else:
                 print("Wrong password")
-                # speak("Wrong password")
+                speak("Wrong password")
         except:
             print("Exception, user not found")
             speak("Exception, user not found")
@@ -105,8 +97,8 @@ def Download_Chords(index):
 
         # download the sound file to a temporary file
         # change to RPi storage path later
-        # temp_file = rf"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp\{note['note']}.wav"
-        temp_file = rf"/home/pi/Desktop/programming/cloudwave/sound/{note['note']}.wav"
+        temp_file = rf"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp\{note['note']}.wav"
+        # temp_file = rf"/home/pi/Desktop/programming/cloudwave/sound/{note['note']}.wav"
         blob = bucket.blob(storage_path)
         blob.download_to_filename(temp_file)
 
@@ -119,7 +111,7 @@ def Download_Chords(index):
 SoundBoard = Download_Chords(index)
 
 # Initialize PyAudio
-p = pyaudio.PyAudio()
+# p = pyaudio.PyAudio()
 
 # Get the index of the Stereo Mix input device (assuming it's available on your computer)
 # input_device_index = None
@@ -134,85 +126,40 @@ p = pyaudio.PyAudio()
 #     print("Stereo Mix input device not found")
 #     exit()
 # Get the index of the onboard audio input device
-input_device_index = None
-info = p.get_host_api_info_by_index(0)
-num_devices = info.get('deviceCount')
-for i in range(num_devices):
-    if "bcm2835" in p.get_device_info_by_host_api_device_index(0, i).get('name').lower():
-        input_device_index = i
 
-# If the onboard audio input device is not available, print an error message and exit
-if input_device_index is None:
-    print("Onboard audio input device not found")
-    exit()
 
-# Open a stream with the Stereo Mix input device
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                input_device_index=input_device_index,
-                frames_per_buffer=CHUNK)
+def change_pitch(sound, pitch):
+    # do for all 12 notes
+    # pitch up or down, 2 is up, 0,5 is down
+    sound.set_speed(pitch)
 
-# get a list of all microphones:v
-# mics = sc.all_microphones(include_loopback=True)
 
-# get the current default microphone on your system:
-# default_mic = mics[1]
-
-# with default_mic.recorder(samplerate=44100) as mic:
-    # frames = []
-    # num_frames = 44100 * 0.5    # record for 5 seconds
-frames = []
-data = 0
-RECORD_SECONDS = 10
-# speak("Recording in progress")
-start_time = pygame.time.get_ticks()
-
+speak("Recording in progress")
 i = 0
-print("next")
+record()
 while True:
     if index != Check_index():
         index = Check_index()
         SoundBoard = Download_Chords(index)
 
-    # data = default_mic.record(numframes=num_frames, samplerate=44100)
-    # frames.append(data)
-    for j in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(44100)
-        frames.append(data)
-        if i < len(Song2):
-            SoundBoard[Song2[i]].play()
-            print(Check_index())
-            i += 1
-        else:
-            break
+    for j in Song2:
+        SoundBoard[Song2[i]].play()
+        print(Check_index())
+        i += 1
+
     break
 # speak("Recording stopped")
 
 # Save the recorded audio in a WAV file
-
 # path = fr"C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp"
-path = fr'/home/pi/Desktop/programming/cloudwave/sound/'
+path = fr'/home/pi/Desktop/programming/cloudwave'
 
-wf = wave.open(fr'{path}\new_song.wav', 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(2)
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-
-# frames = np.concatenate(frames)
-# frames /= 1.414
-# frames *= 32767
-# int16_data = frames.astype(np.int16)
-# write(path, 44100, int16_data)
-
+converter()
 
 # upload to firebase
-# upload_path = rf"{current_user}/Saved_Music/new_song.wav.wav"
+# upload_path = rf"{current_user}/Saved_Music/new_song.wav"
 # blob = bucket.blob(upload_path)
-# blob.upload_from_filename(fr'C:\Users\anton\OneDrive\Dokument\1. Skolsaker\0. Projekt och Projektmetoder\Projekt\temp\new_song.wav')
+# blob.upload_from_filename(fr'{path}\output.wav')
 
 print(f"finished uploading new_song.wav")
 
--- raw-to-wav-converter.py
+++ raw-to-wav-converter.py
@@ -1,16 +1,19 @@
 import wave
 
+path = fr'/home/pi/Desktop/programming/cloudwave'
+
+
 def converter():
-  # Open the raw audio file
-  with open('input.raw', 'rb') as raw_file:
-      raw_data = raw_file.read()
+    # Open the raw audio file
+    with open(fr'{path}\input.raw', 'rb') as raw_file:
+        raw_data = raw_file.read()
 
-  # Create a new WAV file
-  with wave.open('output.wav', 'wb') as wav_file:
-      # Set the parameters for the WAV file
-      wav_file.setnchannels(2)
-      wav_file.setsampwidth(2)
-      wav_file.setframerate(44100)
+    # Create a new WAV file
+    with wave.open(fr'{path}\output.wav', 'wb') as wav_file:
+        # Set the parameters for the WAV file
+        wav_file.setnchannels(2)
+        wav_file.setsampwidth(2)
+        wav_file.setframerate(44100)
 
-      # Write the raw data to the WAV file
-      wav_file.writeframesraw(raw_data)
+        # Write the raw data to the WAV file
+        wav_file.writeframesraw(raw_data)
